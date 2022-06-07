d1c8d5afb8b12bbaf01493361d2c37a32a88661e
aidan-starke <starke.aidan@gmail.com>
2022-06-07 21:20:11 +1200

    setup layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "@/app.css";
+</script>
+
+<svelte:head>
+	<title>CENNZnet Svelte Faucet</title>
+</svelte:head>
+
+<div
+	class="z-0 flex h-screen h-screen items-center overflow-auto bg-[#f5ecff] bg-[url('@/assets/bg.svg')] font-sans"
+>
+	<slot />
+</div>
