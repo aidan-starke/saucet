0f4cfa22ae0babdd29a35a5c15785ef87a25de05
aidan-starke <starke.aidan@gmail.com>
2022-06-07 21:20:34 +1200

    setup store

---
+++ src/stores/faucet.ts
@@ -0,0 +1,6 @@
+import type { Chain, CENNZnetNetwork } from "@/types";
+import { writable } from "svelte/store";
+
+export const address = writable<string>();
+export const addressType = writable<Chain>();
+export const network = writable<CENNZnetNetwork>();\
