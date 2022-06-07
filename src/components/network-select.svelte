c6accef157b5ef429d964802a521709fcadb5fdd
aidan-starke <starke.aidan@gmail.com>
2022-06-07 21:19:51 +1200

    setup selects

---
+++ src/components/network-select.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { CENNZnetNetwork } from "@/types";
+
+	const NIKAU: CENNZnetNetwork = "Nikau";
+	const RATA: CENNZnetNetwork = "Rata";
+
+	let selectedNetwork = "Nikau";
+
+	let networks = [NIKAU, RATA];
+</script>
+
+<div
+	class="m-auto mt-1 block w-5/6 rounded border border-gray-400 pr-1 hover:border-[#9847FF]"
+>
+	<select
+		bind:value={selectedNetwork}
+		class="h-10 w-full cursor-pointer pl-2 text-sm font-bold"
+	>
+		{#each networks as network (network)}}
+			<option value={network}>{network}</option>
+		{/each}
+	</select>
+</div>
---
+++ src/components/token-select.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { SUPPORTED_TOKENS } from "@/constants";
+
+	let selectedToken = "CENNZ";
+</script>
+
+<div
+	class="m-auto mt-1 block w-5/6 rounded border border-gray-400 pr-1 hover:border-[#9847FF]"
+>
+	<select
+		bind:value={selectedToken}
+		class="h-10 w-full cursor-pointer pl-2 text-sm font-bold"
+	>
+		{#each SUPPORTED_TOKENS as token (token.symbol)}}
+			<option value={token.symbol}>{token.symbol}</option>
+		{/each}
+	</select>
+</div>
