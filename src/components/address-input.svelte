a76777a1dda9d737f102696562e041ea77ccc0e2
aidan-starke <starke.aidan@gmail.com>
2022-06-08 13:33:49 +1200

    display tooltip for invalid address

--- src/components/address-input.svelte
+++ src/components/address-input.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { setAddressType, address } from "@/stores/faucet";
+	import { fade } from "svelte/transition";
 	let input = "";
+
+	$: isValidAddress = input ? setAddressType(input) : true;
 
 	$: {
 		address.set(input);
-		const isValidAddress = setAddressType(input);
-		if (!isValidAddress) {
-			// warn user
-		}
 	}
 </script>
 
@@ -18,3 +17,27 @@
 	placeholder="Enter a CENNZnet or Ethereum address"
 	bind:value={input}
 />
+{#if !isValidAddress}
+	<div class="m-auto w-48 text-sm" transition:fade>
+		<div
+			class="align-content-center m-5 flex rounded-md border border-solid border-yellow-400 p-2 text-center shadow danger-arrow-top"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="2"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+				/>
+			</svg>
+
+			<span class="ml-1.5 py-0.5">Invalid address!</span>
+		</div>
+	</div>
+{/if}
