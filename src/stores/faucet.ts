ed5f2b2dae3463d268d9c4917b1ffbe661db779e
aidan-starke <starke.aidan@gmail.com>
2022-06-09 07:08:26 +1200

    derive `cennzAddress`

--- src/stores/faucet.ts
+++ src/stores/faucet.ts
@@ -1,13 +1,20 @@
 import type { Chain, CENNZnetNetwork, CENNZnetToken } from "@/types";
+import type { Writable } from "svelte/store";
 
-import { writable } from "svelte/store";
+import { writable, derived } from "svelte/store";
 import { isCENNZAddress, isEthereumAddress } from "@/utils";
 import { SUPPORTED_TOKENS } from "@/constants";
+import { cvmToAddress } from "@cennznet/types/utils";
 
 export const address = writable<string>();
 export const addressType = writable<Chain>();
 export const network = writable<CENNZnetNetwork>();
 export const token = writable<CENNZnetToken>(SUPPORTED_TOKENS[0]);
+export const cennzAddress = derived<Writable<string>, string>(
+	address,
+	($address) =>
+		isEthereumAddress($address) ? cvmToAddress($address) : $address
+);
 
 export const setAddressType = (address: string) => {
 	if (isCENNZAddress(address)) {
