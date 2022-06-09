5f0438fe4f1cae4340a09e4d53d520a92feffa55
aidan-starke <starke.aidan@gmail.com>
2022-06-09 17:48:10 +1200

    add metamask store and set extension

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import "@/app.css";
+	import { onMount } from "svelte";
+	import detectEthereumProvider from "@metamask/detect-provider";
+	import { extension } from "@/stores/metamask";
+
+	onMount(() => {
+		detectEthereumProvider({ mustBeMetaMask: true }).then(extension.set);
+	});
 </script>
 
 <svelte:head>
