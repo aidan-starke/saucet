41d1f6bdcf4e99e5a037b08d0d4604f3a2d8cb39
aidan-starke <starke.aidan@gmail.com>
2022-06-09 21:57:34 +1200

    i think we gucci

--- src/utils/appAuth.ts
+++ src/utils/appAuth.ts
@@ -30,47 +30,6 @@ export const appAuth = new SvelteKitAuth({
 				return { ...profile, provider: "github" };
 			},
 		}),
-		// new class extends GitHubOAuth2Provider {
-		//   constructor() {
-		//     super({
-		//       clientId: GITHUB_CLIENT_ID,
-		//   clientSecret: GITHUB_CLIENT_SECRET,
-		//   profile(profile) {
-		//     return { ...profile, provider: "github" };
-		//   },
-		//     })
-		//   }
-		//   getRequestToken = async (auth, host): Promise<any> => {
-		//     const { url, ...oauthResult } = await (new TwitterApi({
-		//       appKey: this.config.apiKey,
-		//       appSecret: this.config.apiSecret
-		//     })).readOnly.generateAuthLink(
-		//       encodeURIComponent(this.getCallbackUri()), { authAccessType: 'read' }
-		//     );
-		//     return {
-		//       oauthToken: oauthResult.oauth_token,
-		//       oauthTokenSecret: oauthResult.oauth_token_secret,
-		//       oauthCallbackConfirmed: oauthResult.oauth_callback_confirmed
-		//     };
-		//   }
-		//   getTokens = async (oauthToken: string, oauthVerifier: string): Promise<any> => {
-		//     const endpoint = 'https://api.twitter.com/oauth/access_token';
-
-		//     const data = {
-		//       oauth_consumer_key: this.config.apiKey,
-		//       oauth_token: oauthToken,
-		//       oauth_verifier: oauthVerifier,
-		//     };
-
-		//     const response: any = await fetch(`${endpoint}?${new URLSearchParams(data)}`, { method: 'POST' });
-		//     // This endpoint returns query string like key-value pairs
-		//     // https://developer.twitter.com/en/docs/authentication/api-reference/access_token
-		//     return Object.fromEntries([...(new URLSearchParams(await response.text()))]);
-		//   }
-		//   getCallbackUri(): string {
-		//     return `${settings.AUTH_CALLBACK_URI_PREFIX}twitter`;
-		//   }
-		// },
 	],
 	callbacks: {
 		redirect: (uri) => uri, // Extend or introspect redirect callbacks
