3e2c57f59a67cd892bff8aa1ae802a56a8891908
aidan-starke <starke.aidan@gmail.com>
2022-06-11 19:20:34 +1200

    fix select styles for firefox

--- src/lib/components/NetworkSelect.svelte
+++ src/lib/components/NetworkSelect.svelte
@@ -3,13 +3,8 @@
 	import { network } from "$lib/stores";
 </script>
 
-<div
-	class="m-auto mt-1 block w-5/6 rounded border border-gray-400 pr-1 hover:border-[#9847FF]"
->
-	<select
-		bind:value={$network}
-		class="h-10 w-full cursor-pointer pl-2 text-sm font-bold"
-	>
+<div class="faucet-select-wrapper">
+	<select bind:value={$network} class="faucet-select">
 		{#each Object.keys(NETWORKS) as network (network)}}
 			<option value={network}>{network}</option>
 		{/each}
--- src/lib/components/TokenSelect.svelte
+++ src/lib/components/TokenSelect.svelte
@@ -9,13 +9,8 @@
 	}
 </script>
 
-<div
-	class="m-auto mt-1 block w-5/6 rounded border border-gray-400 pr-1 hover:border-[#9847FF]"
->
-	<select
-		bind:value={selectedToken}
-		class="h-10 w-full cursor-pointer pl-2 text-sm font-bold"
-	>
+<div class="faucet-select-wrapper">
+	<select bind:value={selectedToken} class="faucet-select">
 		{#each SUPPORTED_TOKENS as token (token.symbol)}
 			<option value={token.symbol}>{token.symbol}</option>
 		{/each}
